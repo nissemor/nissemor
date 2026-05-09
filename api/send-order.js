a3dcb785b75274439fb766327bff4984545c1249
nissemor <official.nissemor@gmail.com>
2026-05-09 16:19:23 -0700

    Update send-order.js

--- api/send-order.js
+++ api/send-order.js
@@ -39,7 +39,7 @@ export default async function handler(req, res) {
           language:      body.language      || '',
           story:         body.story         || ''
         },
-        groups: ['186960031518820181'] // NisseMor Orders
+        groups: ['187015975721240041'] // NisseMor Orders
       })
     });
 
